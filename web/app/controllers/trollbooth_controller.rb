eb3ec6646e30eb105b12fdd3d76022918dfe23a6
Kristijan Novoselic <kristijan@glooko.com>
2016-11-06 14:28:03 +0100

    Send open command to the esp826 hardware over MQTT

--- web/app/controllers/trollbooth_controller.rb
+++ web/app/controllers/trollbooth_controller.rb
@@ -3,5 +3,19 @@ class TrollboothController < ApplicationController
   end
 
   def open
+    mqtt do |client|
+      client.publish 'trollbooth', '1'
+    end
+  end
+
+  private
+
+  def mqtt
+    client = MQTT::Client.new
+    client.host = Rails.application.config.mqtt_host
+    client.client_id = 'trollbooth_server'
+    client.connect
+    yield client
+    client.disconnect
   end
 end
