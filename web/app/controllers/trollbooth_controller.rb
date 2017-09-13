5d64985523b34419e034538a54e9c6941164ba30
Kristijan Novoselic <kristijan@glooko.com>
2017-09-13 12:09:13 +0200

    Simplify code

--- web/app/controllers/trollbooth_controller.rb
+++ web/app/controllers/trollbooth_controller.rb
@@ -2,33 +2,29 @@ class TrollboothController < ApplicationController
   ACK_TOPIC = 'trollbooth/acks'.freeze
   REQUEST_TOPIC = 'trollbooth/requests'.freeze
 
-  def index
-  end
+  def index; end
 
   def open
-    open_gate do |success|
-      head(success ? :no_content : :service_unavailable)
-    end
+    head(open_gate ? :no_content : :service_unavailable)
   end
 
   def letmein
-    open_gate do |success|
-      @success = success
-    end
+    @success = open_gate
   end
 
   private
 
   def open_gate
+    message_id = SecureRandom.uuid
+    message = nil
     mqtt do |client|
-      message_id = SecureRandom.uuid
       client.subscribe ACK_TOPIC
       client.publish REQUEST_TOPIC, message_id
       wait_for_acknowledgement client
       _topic, message = client.get unless client.queue_empty?
-
-      yield(message == message_id)
     end
+
+    message == message_id
   end
 
   def mqtt
